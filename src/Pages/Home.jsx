43bf2bcca1083ef36968bd7693229b4d60aca5cd
RobertoMada <robertomadalin18@gmail.com>
2024-06-29 11:17:51 +0300

    plant trip section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import BookCar from "../components/BookCar";
+import PlanTrip from "../components/PlanTrip";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <BookCar />
+      <PlanTrip />
     </>
   );
 };
